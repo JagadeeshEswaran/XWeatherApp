54bbdd95684ec122ec3ddfbeff43c89693f1dfcb
Mohamed Sha <mohsha@rhfl.cbs>
2024-08-01 15:39:16 +0530

    bug fixes 2

--- src/WeatherTile.jsx
+++ src/WeatherTile.jsx
@@ -5,6 +5,7 @@ import React from "react";
 const WeatherTile = ({ disc, data, unit }) => {
   return (
     <article
+      className=".weather-cards"
       style={{
         height: "6rem",
         width: "12rem",
